d26c47f6f4d443388b036ca90a78839969c40be8
carisbe1777 <carisbe066@gmail.com>
2025-07-23 17:29:27 +0000

    finalizado

---
+++ models/calculadora.js
@@ -0,0 +1,10 @@
+const calculadora={
+  subtracao: function(n1,n2){
+    return n1 - n2
+  },
+  soma: function(n1,n2){
+    return n1+ n2
+  }
+}
+
+module.exports = calculadora
--- tests/calculadora.test.js
+++ tests/calculadora.test.js
@@ -1,7 +1,17 @@
 // @ts-check
 
-
+const calculadora = require("../models/calculadora")
 
 test("criando primeiro teste", function(){
   console.log("oie")
+});
+
+test("Soma de 2 + 2 deve retornar 4",function(){
+ const resultado = calculadora.soma(2,2)
+ expect(resultado).toBe(4)
+});
+
+test("soma de 2 + 4 deve retornar 6",function(){
+  const resultado = calculadora.soma(2,4)
+  expect(resultado).toBe(6)
 })
